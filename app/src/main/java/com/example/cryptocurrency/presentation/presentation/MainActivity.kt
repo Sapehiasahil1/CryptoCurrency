f47a627233ae95851f0515d1f4ec0d850b2c0188
Sapehiasahil1 <officialsahilsapehia03@gmail.com>
2023-07-03 19:44:11 +0530

    CoinDetailScreen

--- app/src/main/java/com/example/cryptocurrency/presentation/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrency/presentation/presentation/MainActivity.kt
@@ -7,19 +7,37 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cryptocurrency.presentation.presentation.coin_detail.CoinDetailScreen
+import com.example.cryptocurrency.presentation.presentation.coin_list.CoinListScreen
 import com.example.cryptocurrency.presentation.presentation.ui.CryptoCurrencyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoCurrencyTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ){
+                            CoinListScreen(navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ){
+                            CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
